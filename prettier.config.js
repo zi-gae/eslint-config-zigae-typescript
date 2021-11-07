2668842290b3b60cab04622c733d171a5b9ec5f5
geonwoo.jeong <doscmdev@gmail.com>
2021-11-07 17:32:09 +0900

    style: add prettier

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: false,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+};
